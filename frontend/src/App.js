6b225f56b9dbf895c31b795389b93974cd7224ee
Hisashi Ishihara <149908441+IshiHisashi@users.noreply.github.com>
2024-05-09 21:53:46 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -78,10 +78,10 @@ if ("serviceWorker" in navigator) {
     })
     .then(function (subscription) {
       console.log("User is subscribed:", subscription);
-      window.location.reload();
     })
     .catch(function (err) {
       console.log("Service worker registration failed, error:", err);
+      window.location.reload();
     });
 }
 async function save(userId) {
